9cf1e8cf96aba29b7fd93b1867d0e860d15bd7f0
KhanhDuy <kduy410@gmail.com>
2020-04-03 21:06:46 +0700

    Update preprocessing function

--- Sequential.py
+++ Sequential.py
@@ -1,6 +1,9 @@
 import os
 
+from skimage.exposure import rescale_intensity
+import argparse
 import dlib
+from align import AlignDlib
 import traceback
 import sys
 import tensorflow as tf
@@ -19,6 +22,8 @@ import mtcnn
 import numpy as np
 import pandas as pd
 from glob import glob
+import scipy
+from scipy import ndimage
 import skimage
 from skimage.transform import rescale, resize, downscale_local_mean
 from tensorflow_core.python.keras.optimizers import SGD
@@ -38,6 +43,7 @@ print("MTCNN-version :", mtcnn.__version__)
 
 DATA_DIR = r"D:\Data\train"
 DATA_TEST_DIR = r"D:\Data\test"
+alignment = AlignDlib('weights/shape_predictor_68_face_landmarks.dat')
 
 
 # Các bước load ảnh và chuẩn hóa trước khi cho vào mạng.
@@ -88,29 +94,39 @@ def create_data_frame(data_path):
 
 
 # mnist_1 = keras_vggface.vggface
-def create_training_data_sequential(DATA_DIR, IMG_SIZE, data_name, label_name):
-    df_train = create_data_frame(DATA_DIR)
-
-    print(df_train.head())
-
+def create_training_data(directory_path, data_name, label_name, required_size=(224, 224)):
+    # CREATE TRAINING DATA FROM PREPROCESSED DATA
+    df_train = create_data_frame(directory_path)
     training_data = []
 
+    print(f"\n{df_train.head()}")
     print(f"\nINFO={df_train.info()}")
     print(f"\nLENGTH={len(df_train)}")
     print(f"\nSHAPE={df_train.shape}")
 
     for index, row in tqdm(df_train.iterrows()):
         try:
-            img_array = imread(row['image'])
-            if img_array.ndim is 2:
-                img_gray = cv2.imread(row['image'], cv2.IMREAD_GRAYSCALE)
-                img_array = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
-                resized = prewhiten(cv2.resize(img_array, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA))
-            else:
-                resized = prewhiten(resize(img_array, (IMG_SIZE, IMG_SIZE)))
-            # TEMPORARY USE GRAY_SCALE, COLOR DON'T WORK
-            resized = skimage.color.rgb2gray(resized)
-            training_data.append([resized, row['label']])
+            img = Image.open(row['image'])
+            img.load()
+            img_array = np.array(img).astype(dtype=np.uint8)
+
+            if type(img_array) is np.ndarray:
+                if img_array.shape[-1] != 3:
+                    if img_array.shape[-1] == 4:  # RGBA
+                        img_array = cv2.cvtColor(img_array, cv2.COLOR_RGBA2RGB)
+                        print(f"CONVERTED={img_array.shape}")
+                    else:  # GRAYSCALE
+                        img_array = cv2.cvtColor(img_array, cv2.COLOR_GRAY2RGB)
+                        print(f"CONVERTED={img_array.shape}")
+                if img_array.shape[:2] != required_size:
+                    print(f"SHAPE DON'T MATCH={img_array.shape}")
+                    img_array = cv2.addWeighted(img_array, 1.5, img_array, -0.5, 0, img_array)
+                    img_array = cv2.normalize(img_array, required_size, 0, 255, cv2.NORM_MINMAX)
+                    img_array = cv2.resize(img_array, required_size, interpolation=cv2.INTER_LINEAR)
+                    print(f"SHAPE RESIZED={img_array.shape}")
+
+                training_data.append([img_array, row['label']])
+
         except Exception as e:
             print(f"ERROR-{row['image']}")
             print(e)
@@ -120,7 +136,7 @@ def create_training_data_sequential(DATA_DIR, IMG_SIZE, data_name, label_name):
             pass
 
     print(f"TRAINING DATA LENGTH: {len(training_data)}")
-    num_classes = np.amax(np.array(training_data)[:, 1])
+    num_classes = np.amax(np.array(training_data)[:, 1]) + 1
     print(f"CLASS NUMBER: {num_classes}")
     random.shuffle(training_data)
 
@@ -131,15 +147,8 @@ def create_training_data_sequential(DATA_DIR, IMG_SIZE, data_name, label_name):
         x_trains.append(features)
         y_trains.append(label)
 
-    # 1 for gray-scale
-    # 3 for color
-    # convert to array only work with gray_scale
     x_trains = np.array(x_trains)
-    print(x_trains.shape)
-    x_trains = x_trains.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-    print(x_trains.shape)
     y_trains = np.array(y_trains)
-
     print(f"X:{x_trains.shape}")
     print(f"Y:{y_trains.shape}")
 
@@ -147,54 +156,91 @@ def create_training_data_sequential(DATA_DIR, IMG_SIZE, data_name, label_name):
     np.save(f'{label_name}', y_trains)
 
 
-def extracting_face_from_image(required_size=(224, 224)):
+# Display one image
+def display_one(a, title1="Original"):
+    plt.imshow(a), plt.title(title1)
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+# Display two images
+def display(a, b, title1="Original", title2="Edited"):
+    plt.subplot(121), plt.imshow(a), plt.title(title1)
+    plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(b), plt.title(title2)
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+def align_face(face, image_size):
+    (h, w, c) = face.shape
+    bb = dlib.rectangle(0, 0, w, h)
+    return alignment.align(image_size, face, bb, landmarkIndices=AlignDlib.INNER_EYES_AND_BOTTOM_LIP)
+
+
+# Preprocessing
+def preprocessing(directory_path, save_path, required_size=(224, 224)):
     # create the detector, using default weights
+
     detector = mtcnn.MTCNN()
-    # hog = dlib.get_frontal_face_detector()
-    # cnn = dlib.cnn_face_detection_model_v1('./weights/mmod_human_face_detector.dat')
-    train_paths = glob(fr"{DATA_DIR}\*")
+    train_paths = glob(fr"{directory_path}\*")
 
     for path in tqdm(train_paths):
         name = path.split("\\")[-1]
         images = glob(f"{path}\\*")
         for image_path in tqdm(images):
             try:
-                # print(image_path)
-                # load image from file
-                image = imread(image_path)
                 temp_path = image_path.split("\\")[-1]
+                # load image from file
+                image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
 
-                # detect faces in the image
+                # ----------------------------------
+                # Convert to RGB
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+                # ----------------------------------
+                # Remove noise
+                # Gaussian
+                image = cv2.GaussianBlur(image, (5, 5), 0)
+
+                # ----------------------------------
+                # Denoising of image saving it into dst image
+                image = cv2.addWeighted(image, 1.5, image, -0.5, 0, image)
+
+                # ----------------------------------
+                # Face Cropping
+                # Detect faces in the image
                 face_recs = detector.detect_faces(image)
-                # face_recs = hog(image, 0)
-                # face_rec = cnn(image, 1)
 
-                # extract the bounding box from the first face
+                # Extract the bounding box from the first face
                 x1, y1, width, height = face_recs[0]['box']
                 x2, y2 = x1 + width, y1 + height
 
-                # for hog or cnn, if it is cnn the face_rec.rect.left()...
-                face_rec = face_recs[0]['box']
-                # face_rec = face_recs[0]
-
-                # if face_rec is None:
-                #     continue
-                #
-                # x1 = face_rec.left()
-                # y1 = face_rec.top()
-                # x2 = face_rec.right()
-                # y2 = face_rec.bottom()
-
-                # extract the face
+                # Extract the face
                 face = image[y1:y2, x1:x2]
-                # resize pixels to the model size
-                image = Image.fromarray(face)
-                image = image.resize(required_size)
-                face = np.asarray(image)
 
-                if not os.path.exists(fr"D:/Data/temp/{name}"):
-                    os.mkdir(fr"D:/Data/temp/{name}")
-                imsave(fr"D:/Data/temp/{name}/{temp_path}", face, format='JPG')
+                # NORMALIZE
+                # --------------------------------
+                norm_face = cv2.normalize(face, required_size, 0, 255, cv2.NORM_MINMAX)
+
+                # ----------------------------------
+                # Face straightening
+                face_rotated = align_face(norm_face, required_size[0])
+                face_rotated = (face_rotated / 255.).astype(np.float32)
+
+                # RESIZE
+                # --------------------------------
+                face_rotated = cv2.resize(face_rotated, required_size, interpolation=cv2.INTER_LINEAR)
+                # Convert to array
+                # --------------------------------
+                face = np.asarray(face_rotated)
+                face = tf.image.convert_image_dtype(face, dtype=tf.uint8, saturate=False)
+
+                if not os.path.exists(fr"{save_path}/{name}"):
+                    os.mkdir(fr"{save_path}/{name}")
+                    imsave(fr"{save_path}/{name}/{temp_path}", face, format='JPEG')
+                else:
+                    imsave(fr"{save_path}/{name}/{temp_path}", face, format='JPEG')
             except Exception as e:
                 print(e)
                 exc_info = sys.exc_info()
@@ -207,8 +253,8 @@ def new_sequential_model():
     NAME = f"People-cnn-64x2-{int(time.time())}"
     tensorboard = TensorBoard(log_dir=f'logs\{NAME}')
 
-    x_trains = np.load('x_test.npy')
-    y_trains = np.load('y_test.npy')
+    x_trains = np.load('x_train.npy')
+    y_trains = np.load('y_train.npy')
 
     print(f"X-SHAPE:{x_trains.shape},\nX-DTYPE: {x_trains.dtype}")
     print(f"Y-SHAPE:{y_trains.shape},\nY-DTYPE: {y_trains.dtype}")
@@ -219,6 +265,7 @@ def new_sequential_model():
     # x_trains = l2_normalize(x_trains)
 
     model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
         Flatten(),
         Dense(64, input_shape=x_trains.shape[1:], activation='relu'),
         Dense(128, activation='relu'),
@@ -231,17 +278,8 @@ def new_sequential_model():
     # evaluate the model
     scores = model.evaluate(x_trains, y_trains, verbose=1)
     print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
-    # serialize weights to HDF5
-    # model.save_weights("sequential.h5")
-    # print("Saved model to disk")
-    x_test = np.load('x_test.npy')
-    y_test = np.load('y_test.npy')
-    predictions = model.predict_classes(x_test, batch_size=16, verbose=1)
-    for i in predictions:
-        print(i)
-    cm = confusion_matrix(y_test, predictions)
-    cm_plot_labels = ['no side effects', 'had side effects']
-    plot_confusion_matrix(cm,cm_plot_labels,predictions)
+
+    model.save('model.h5')
 
 
 def sequential_model():
@@ -287,12 +325,16 @@ def sequential_model():
 
 
 def main():
-    # extracting_face_from_image()
+    # preprocessing(r"D:\Data\test", r"D:\Data\test")
     # create_training_data_sequential(DATA_DIR, 224, 'x_train', 'y_train')
-    # create_training_data_sequential(DATA_TEST_DIR, 224, 'x_test', 'y_test')
+    # create_training_data(DATA_DIR, 'x_train', 'y_train')
+    create_training_data(DATA_TEST_DIR, 'x_test', 'y_test')
 
-    new_sequential_model()
-
+    # new_sequential_model()
+    # model = load_model('model.h5')
+    # model.summary()
+    # model.get_weights()
+    # model.optimizer
     # model = t('sequential.h5')
     # # Prediction  - predict always take a list
     # test = imread("D:/Data/test/irene.jpg")
